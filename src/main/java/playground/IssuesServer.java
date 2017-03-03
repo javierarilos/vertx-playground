6567a241c59443497ff4d93a31283f4a08bf0c6f
javierarilos <javier.arilos@gmail.com>
2017-03-03 16:42:34 +0100

    IssueServer first inmemory-JsonObject-based version

--- src/main/java/playground/IssuesServer.java
+++ src/main/java/playground/IssuesServer.java
@@ -5,65 +5,68 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class IssuesServer {
-public static Map<String, Object> issues = new HashMap<>();
+  public static Map<String, JsonObject> issues = new HashMap<>();
 
-  public static JsonObject getIssueJson(String userId) {
-    Object issueDesc = issues.getOrDefault(userId, "");
-    return new JsonObject()
-                .put("user_id", userId)
-                .put("issue_desk", issueDesc);
+  public static void putIssue(String userId, String issueDesc){
+    issues.put(userId, new JsonObject().put("user_id", userId).put("issue_desc", issueDesc));
   }
 
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
 
-    issues.put("john", "router KO");
-    issues.put("lucy", "phone power button broken");
-
-    Handler<RoutingContext> sayHiHandler = routingContext -> {
-      HttpServerResponse resp = routingContext.response();
-      resp.putHeader("Content-Type", "text/plain");
-      resp.end("Hello from Vert.x-Web");
-    };
-
+    putIssue("john", "router KO");
+    putIssue("lucy", "phone power button broken");
 
     Handler<RoutingContext> getAllIssuesHandler = routingContext -> {
+      JsonArray allIssuesJson = new JsonArray(new ArrayList(issues.values()));
+
       routingContext.response()
           .setStatusCode(200)
           .putHeader("Content-Type", "application/json")
-          .end(new JsonObject(issues).encode());
+          .end(allIssuesJson.encode());
     };
 
     Handler<RoutingContext> getIssueHandler = routingContext -> {
       String userId = routingContext.request().getParam("userId");
-      routingContext.response()
-          .setStatusCode(200)
-          .putHeader("Content-Type", "application/json")
-          .end(getIssueJson(userId).encode());
+      JsonObject issueJson = issues.getOrDefault(userId, null);
+
+      if (issueJson == null) {
+        routingContext.response()
+            .setStatusCode(404)
+            .putHeader("Content-Type", "application/json")
+            .end();
+      } else {
+        routingContext.response()
+            .setStatusCode(200)
+            .putHeader("Content-Type", "application/json")
+            .end(issueJson.encode());
+
+      }
     };
 
-    Handler<RoutingContext> newIssueHandler  = routingContext -> {
+    Handler<RoutingContext> newIssueHandler = routingContext -> {
       /*  Body is in json in the form:
           {"user_id": "martha", "issue_desc": "some problem"}
       */
       JsonObject body = routingContext.getBodyAsJson();
       String userId = body.getString("user_id");
-      String issueDesc = body.getString("issue_desc");
-      issues.put(userId, issueDesc);
+      issues.put(userId, body);
 
       routingContext.response()
           .setStatusCode(201)
           .putHeader("Content-Type", "application/json")
-          .end(getIssueJson(userId).encode());
+          .end(body.encode());
     };
 
     Router router = Router.router(vertx);
@@ -71,7 +74,6 @@ public static Map<String, Object> issues = new HashMap<>();
     router.route(HttpMethod.GET, "/issue").handler(getAllIssuesHandler);
     router.route(HttpMethod.GET, "/issue/:userId").handler(getIssueHandler);
     router.route(HttpMethod.POST, "/issue").handler(newIssueHandler);
-    router.route().handler(sayHiHandler);
 
 
     HttpServer server = vertx.createHttpServer();
